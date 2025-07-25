757d500badb1e80d0f75e9b2a8780ec8c998b450
Артур Мыльников <kuraidatenshidesu@gmail.com>
2025-07-25 16:20:45 +0300

    Added edit option to the Project title

--- src/components/Board/ui/Board.tsx
+++ src/components/Board/ui/Board.tsx
@@ -1,16 +1,57 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 
 import style from "./Board.module.css";
 import { Column } from "../../Column";
 
 const Board: FC = () => {
+  const [title, setTitle] = useState("Project title");
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [editValue, setEditValue] = useState(title);
+
+  const handleEdit = () => {
+    setIsEdit(false);
+
+    const newTitle = editValue.trim();
+
+    if (newTitle) {
+      setTitle(newTitle);
+      setEditValue(newTitle);
+    } else {
+      setEditValue(title);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    handleEdit();
+  };
+
   return (
     <div className={style.Board}>
       <div className={style.Heading}>
-        <h1 className={style.Title}>Project Title</h1>
-        <button className={style.Edit}>
-          <span className="material-symbols-outlined">edit</span>
-        </button>
+        {isEdit && (
+          <form className={style.Edit} onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className={style.Input}
+              placeholder="Project title..."
+              value={editValue}
+              onChange={(e) => setEditValue(e.target.value)}
+              autoFocus
+              onBlur={handleEdit}
+            />
+          </form>
+        )}
+        {!isEdit && (
+          <>
+            <h1 className={style.Title}>{title}</h1>
+            <button className={style.Button} onClick={() => setIsEdit(true)}>
+              <span className="material-symbols-outlined">edit</span>
+            </button>
+          </>
+        )}
       </div>
       <div className={style.Filters}>
         <button className={style.Filter}>
